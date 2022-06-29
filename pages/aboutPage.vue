4bc22530349b13a8eaa7ee226d5ac295602c4cf4
fangyi <fangyifangyif@126.com>
2022-06-29 22:28:15 +0800

    code commit

---
+++ pages/aboutPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about-container">
+    This about pages
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AboutPage',
+    data() {
+      return {}
+    }
+  }
+</script>
